0d7dc9d6bb5ec1b365c406795325b517658cab76
Nicolas Naranjo <naranjon@google.com>
2020-07-06 08:07:11 -0500

    Literal types and type aliases

---
+++ getting started/literal-types.ts
@@ -0,0 +1,15 @@
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+console.log(combine(1, "hello", "as-text"));
---
+++ getting started/type-aliases.ts
@@ -0,0 +1,44 @@
+type Combinable = number | string;
+type ConversionText = "as-number" | "as-text";
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionText
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+console.log(combine(1, "hello", "as-text"));
+
+// example
+
+// from
+function greet(user: { name: string; age: number }) {
+  console.log('Hi, I am ' + user.name);
+}
+
+function isOlder(user: { name: string; age: number }, checkAge: number) {
+  return checkAge > user.age;
+}
+
+// to
+
+type User = { name: string; age: number };
+
+function greetRes(user: User) {
+  console.log('Hi, I am ' + user.name);
+}
+
+function isOlderRes(user: User, checkAge: number) {
+  return checkAge > user.age;
+}
